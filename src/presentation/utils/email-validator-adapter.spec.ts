7ff7e9af5f46f4d90390869a3799eea810cfe19f
dominicsc2 <dominicsc2hs@gmail.com>
2022-01-26 18:35:09 -0600

    refactor: move sut creation to a factory helper method

--- src/presentation/utils/email-validator-adapter.spec.ts
+++ src/presentation/utils/email-validator-adapter.spec.ts
@@ -1,5 +1,5 @@
-import { EmailValidatorAdapter } from './email-validator';
 import validator from 'validator';
+import { EmailValidatorAdapter } from './email-validator';
 
 jest.mock('validator', () => ({
     isEmail (): boolean {
@@ -7,9 +7,13 @@ jest.mock('validator', () => ({
     }
 }));
 
+const makeSut = (): EmailValidatorAdapter => {
+    return new EmailValidatorAdapter()
+}
+
 describe('EmailValidator Adapter', () => {
     test('Should return false if validator returns false', () => {
-        const sut = new EmailValidatorAdapter();
+        const sut = makeSut();
         jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false); // we don't care how this library validates emails
         // we shouldn't be concerned about, because that is the reason for which we use a wrapper on that library (adapter)
         // EmailValidatorAdapter
@@ -18,13 +22,13 @@ describe('EmailValidator Adapter', () => {
     });
 
     test('Should return true if validator returns true', () => {
-        const sut = new EmailValidatorAdapter();
+        const sut = makeSut();
         const isValid = sut.isValid('valid_email@mail.com');
         expect(isValid).toBe(true);
     });
 
     test('Should call validator with correct email', () => {
-        const sut = new EmailValidatorAdapter();
+        const sut = makeSut();
         const isEmailSpy = jest.spyOn(validator, 'isEmail');
         sut.isValid('any_email@mail.com');
         expect(isEmailSpy).toHaveBeenCalledWith('any_email@mail.com');
