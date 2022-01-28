5d119b79358df9a8d3ac2abf7249b33d9eab60b9
dominicsc2 <dominicsc2hs@gmail.com>
2022-01-27 20:28:10 -0600

    refactor: clear database between tests

--- src/infrastructure/db/mongodb/account-repository/account.spec.ts
+++ src/infrastructure/db/mongodb/account-repository/account.spec.ts
@@ -8,6 +8,10 @@ describe('Account Mongo Repository', () => {
   afterAll(async () => {
     await MongoHelper.disconnect();
   })
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts');
+    await accountCollection.deleteMany({}); // delete objects in memory so that tests don't overlap
+  })
 
   const makeSut = (): AccountMongoRepository => {
     return  new AccountMongoRepository()
