a9cdedecf9005fbdf552039ea3bf8f6dcc2238d9
dominicsc2 <dominicsc2hs@gmail.com>
2022-02-12 12:42:21 -0600

    feat: ensure LoginController return 401 if invalid credentials are provided

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,9 +1,14 @@
-import { ServerError } from '@presentation/errors';
+import { ServerError, UnauthorizedError } from '@presentation/errors';
 import { httpResponse } from '@presentation/protocols';
 
 export const badRequest = (error: Error): httpResponse => ({
   statusCode: 400,
   body: error,
+});
+
+export const unauthorized = (): httpResponse => ({
+  statusCode: 401,
+  body: new UnauthorizedError(),
 });
 
 export const serverError = (error: Error): httpResponse => ({
