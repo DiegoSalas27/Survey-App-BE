46bf7aa70278b86d74126ccd9e0faf279843146a
dominicsc2 <dominicsc2hs@gmail.com>
2022-02-16 20:37:34 -0600

    feat: ensure DbAuthentication calls HashComparer with correct values

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,18 +1,25 @@
+import { HashComparer } from '@data/protocols/cryptography/hash-comparer'
 import { LoadAccountByEmailRepository } from '@data/protocols/db/load-account-by-email-repository'
 import { Authentication, AuthenticationModel } from '@domain/usecases/authentication'
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
+  private readonly hashComparer: HashComparer
 
-  constructor(loadAccountByEmailRepository: LoadAccountByEmailRepository) {
+  constructor(loadAccountByEmailRepository: LoadAccountByEmailRepository, hashComparer: HashComparer) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
+    this.hashComparer = hashComparer
   }
 
   // in case en error is return from a dependency, we don't want to use try and catch,
   // because we want the error to propagate so that the controller can capture it and
   // do something with it
   async auth(authentication: AuthenticationModel): Promise<string> {
-    await this.loadAccountByEmailRepository.load(authentication.email)
+    const account = await this.loadAccountByEmailRepository.load(authentication.email)
+    if (account) {
+      await this.hashComparer.compare(authentication.password, account.password)
+    }
+
     return null
   }
 }
