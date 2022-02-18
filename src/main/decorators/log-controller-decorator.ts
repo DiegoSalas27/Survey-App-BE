f45d7a0cf0a2391f0f2d9b1798ef6a4cb121e762
dominicsc2 <dominicsc2hs@gmail.com>
2022-02-17 22:46:15 -0600

    refactor: remove duplicated code

--- src/main/decorators/log-controller-decorator.ts
+++ src/main/decorators/log-controller-decorator.ts
@@ -2,19 +2,13 @@ import { LogErrorRepository } from '@data/protocols/db/log/log-error-repository'
 import { Controller, HttpRequest, httpResponse } from '@presentation/protocols'
 
 export class LogControllerDecorator implements Controller {
-  private readonly controller: Controller
-  private readonly logErrorRepository: LogErrorRepository
-
-  constructor(controller: Controller, logErrorRepository: LogErrorRepository) {
-    this.controller = controller
-    this.logErrorRepository = logErrorRepository
-  }
+  constructor(private readonly controller: Controller, private readonly logErrorRepository: LogErrorRepository) {}
 
   async handle(httpRequest: HttpRequest): Promise<httpResponse> {
-    const httpResponse = await this.controller.handle(httpRequest);
+    const httpResponse = await this.controller.handle(httpRequest)
     if (httpResponse.statusCode === 500) {
       await this.logErrorRepository.logError(httpResponse.body.stack)
     }
-    return httpResponse;
+    return httpResponse
   }
 }
--- src/presentation/controller/signup/signup-controller.ts
+++ src/presentation/controller/signup/signup-controller.ts
@@ -2,13 +2,7 @@ import { badRequest, ok, serverError } from '@presentation/helpers/http/http-hel
 import { AddAccount, Controller, HttpRequest, httpResponse, Validation } from './signup-controller-protocols'
 
 export class SignUpController implements Controller {
-  private readonly addAccount: AddAccount
-  private readonly validation: Validation
-
-  constructor(addAccount: AddAccount, validation: Validation) {
-    this.addAccount = addAccount
-    this.validation = validation
-  }
+  constructor(private readonly addAccount: AddAccount, private readonly validation: Validation) {}
 
   async handle(httpRequest: HttpRequest): Promise<httpResponse> {
     try {
