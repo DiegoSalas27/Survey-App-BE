7c4a40ba938af024e6dfedbb62cadc71b26e32bb
dominicsc2 <dominicsc2hs@gmail.com>
2022-02-21 17:12:11 -0600

    refactor: change routes to use dynamic extension based on environment

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,12 +1,22 @@
 import { Express, Router } from 'express'
 import fg from 'fast-glob'
+// import { readdirSync } from 'fs'
 
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-  fg.sync('**/src/main/routes/**routes.ts').map(async file =>
+
+  const ext = process.env.NODE_ENV === 'production' ? 'js' : 'ts'
+
+  fg.sync(`**/src/main/routes/**routes.${ext}`).map(async file =>
     // const route = (await import(`../../../${file}`)).default
     // route(router)
     (await import(`../../../${file}`)).default(router)
   )
+
+  // readdirSync(`${__dirname}/../routes`).map(async file => {
+  //   if (file.includes('.test.')) {
+  //     (await import(`../routes/${file}`)).default(router)
+  //   }
+  // })
 }
