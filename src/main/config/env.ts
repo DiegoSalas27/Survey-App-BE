16e9e0822b7970c5b2048cd467b83bfa5c55ca3e
dominicsc2 <dominicsc2hs@gmail.com>
2022-02-21 18:36:39 -0600

    chore: updated environment variables and Procfile for deployment

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,6 @@
 export default {
-    mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
+    mongoUrl: 'mongodb+srv://m001-student:m001-student@sandbox.a20gi.mongodb.net/clean-node-api' ||
+    'mongodb://localhost:27017/clean-node-api',
     port: process.env.PORT || 5050,
     jwtSecret: process.env.JWT_SECRET || 'tj67=Aswe'
 }
