dc1e1d9a1e049784707c22df98b15e3b2f30fa0b
dominicsc2 <dominicsc2hs@gmail.com>
2022-02-23 19:29:51 -0600

    feat: ensure DbAddAccount calls LoadAccountByEmailRepository with correct email

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,9 +1,21 @@
-import { AccountModel, AddAccount, AddAccountModel, AddAccountRepository, Hasher } from './db-add-account-protocols'
+import {
+  AccountModel,
+  AddAccount,
+  AddAccountModel,
+  AddAccountRepository,
+  Hasher,
+  LoadAccountByEmailRepository
+} from './db-add-account-protocols'
 
 export class DbAddAccount implements AddAccount {
-  constructor(private readonly hasher: Hasher, private readonly addAccountRepository: AddAccountRepository) {}
+  constructor(
+    private readonly hasher: Hasher,
+    private readonly addAccountRepository: AddAccountRepository,
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
+  ) {}
 
   async add(accountData: AddAccountModel): Promise<AccountModel> {
+    await this.loadAccountByEmailRepository.loadByEmail(accountData.email)
     const hashedPassword = await this.hasher.hash(accountData.password)
     // the first empty object makes sure we don't modify the accountData value, we just pass an object with an addicional
     // or overwritten property value, but not modifying the actual accountData object
