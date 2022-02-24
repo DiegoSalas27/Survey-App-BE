f7d343875d8fc7523fc528b6af8cc5e2b7627cb2
dominicsc2 <dominicsc2hs@gmail.com>
2022-02-23 19:18:31 -0600

    refactor: separate factories in folders

---
+++ src/main/factories/usecases/authentication/db-authentication-factory.ts
@@ -0,0 +1,14 @@
+import { DbAuthentication } from '@data/usecases/authentication/db-authentication'
+import { Authentication } from '@domain/usecases/authentication'
+import { BcryptAdapter } from '@infrastructure/cryptography/bcrypt-adapter/bcrypt-adapter'
+import { JwtAdapter } from '@infrastructure/cryptography/jwt-adapter/jwt-adapter'
+import { AccountMongoRepository } from '@infrastructure/db/mongodb/account/account-mongo-repository'
+import env from '@main/config/env'
+
+export const makeDbAuthentication = (): Authentication => {
+  const salt = 12
+  const accountMongoRepository = new AccountMongoRepository()
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const jwtAdapter = new JwtAdapter(env.jwtSecret)
+  return new DbAuthentication(accountMongoRepository, bcryptAdapter, jwtAdapter, accountMongoRepository)
+}
