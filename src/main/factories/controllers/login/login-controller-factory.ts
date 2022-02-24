4e5021fbdf64c441c7b51b414c4ffeee000c17dd
dominicsc2 <dominicsc2hs@gmail.com>
2022-02-24 11:54:06 -0600

    refactor: move login and signup controllers to a folder

--- src/main/factories/controllers/login/login-controller-factory.ts
+++ src/main/factories/controllers/login/login-controller-factory.ts
@@ -1,6 +1,6 @@
 import { makeLogControllerDecorator } from '@main/factories/decorators/log-controller-decorator-factory'
 import { makeDbAuthentication } from '@main/factories/usecases/authentication/db-authentication-factory'
-import { LoginController } from '@presentation/controller/login/login-controller'
+import { LoginController } from '@presentation/controller/auth/login/login-controller'
 import { Controller } from '@presentation/protocols'
 import { makeLoginValidation } from './login-validation-factory'
 
