ec0e8ca1c9a1e88d7e0a35100a0bc8bd9d5866e2
dominicsc2 <dominicsc2hs@gmail.com>
2022-02-25 17:57:11 -0600

    feat: ensure LoadSurveysController returns 500 if LoadSurveysUseCase throws

--- src/presentation/controller/survey/load-surveys/load-surveys-controller.ts
+++ src/presentation/controller/survey/load-surveys/load-surveys-controller.ts
@@ -1,11 +1,15 @@
-import { ok } from '@presentation/middlewares/auth-middleware-protocols'
+import { ok, serverError } from '@presentation/middlewares/auth-middleware-protocols'
 import { Controller, HttpRequest, httpResponse, LoadSurveys } from './load-surveys-controller-protocols'
 
 export class LoadSurveysController implements Controller {
   constructor(private readonly loadSurveys: LoadSurveys) {}
 
   async handle(httpRequest: HttpRequest): Promise<httpResponse> {
-    const surveys = await this.loadSurveys.load()
-    return ok(surveys)
+    try {
+      const surveys = await this.loadSurveys.load()
+      return ok(surveys)
+    } catch (error: any) {
+      return serverError(error)
+    }
   }
 }
