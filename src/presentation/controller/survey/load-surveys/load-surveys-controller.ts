23d10c0416ced8ec91c808bba005400658a9ef47
dominicsc2 <dominicsc2hs@gmail.com>
2022-02-25 17:54:52 -0600

    feat: ensure LoadSurveysController returns 200 on success

--- src/presentation/controller/survey/load-surveys/load-surveys-controller.ts
+++ src/presentation/controller/survey/load-surveys/load-surveys-controller.ts
@@ -1,10 +1,11 @@
+import { ok } from '@presentation/middlewares/auth-middleware-protocols'
 import { Controller, HttpRequest, httpResponse, LoadSurveys } from './load-surveys-controller-protocols'
 
 export class LoadSurveysController implements Controller {
   constructor(private readonly loadSurveys: LoadSurveys) {}
 
   async handle(httpRequest: HttpRequest): Promise<httpResponse> {
-    await this.loadSurveys.load()
-    return null
+    const surveys = await this.loadSurveys.load()
+    return ok(surveys)
   }
 }
