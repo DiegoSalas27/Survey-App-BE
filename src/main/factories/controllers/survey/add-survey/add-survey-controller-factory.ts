2d15e3ce84029644892fa4bb8a0062d78c924895
dominicsc2 <dominicsc2hs@gmail.com>
2022-02-24 21:32:21 -0600

    refactor: organize main layer folders

--- src/main/factories/controllers/survey/add-survey/add-survey-controller-factory.ts
+++ src/main/factories/controllers/survey/add-survey/add-survey-controller-factory.ts
@@ -1,5 +1,5 @@
 import { makeLogControllerDecorator } from '@main/factories/decorators/log-controller-decorator-factory'
-import { makeDbAddSurvey } from '@main/factories/usecases/add-survey/db-add-account-factory'
+import { makeDbAddSurvey } from '@main/factories/usecases/survey/add-survey/db-add-account-factory'
 import { AddSurveyController } from '@presentation/controller/survey/add-survey/add-survey-controller'
 import { Controller } from '@presentation/protocols'
 import { makeAddSurveyValidation } from './add-survey-validation-factory'
--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -1,5 +1,5 @@
-import { adaptRoute } from '@main/adapters/express/express-route-adapter'
-import { makeAddSurveyController } from '@main/factories/controllers/add-survey/add-survey-controller-factory'
+import { adaptRoute } from '@main/adapters/express-route-adapter'
+import { makeAddSurveyController } from '@main/factories/controllers/survey/add-survey/add-survey-controller-factory'
 import { Router } from 'express'
 
 export default (router: Router): void => {
