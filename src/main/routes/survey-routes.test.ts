13ef129acf6353a41e9a910079e2a7e27c4e624b
dominicsc2 <dominicsc2hs@gmail.com>
2022-02-25 09:06:59 -0600

    test: ensure add survey routes returns 204 if valid access  token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -1,9 +1,13 @@
+import request from 'supertest'
+import { sign } from 'jsonwebtoken'
 import { MongoHelper } from '@infrastructure/db/mongodb/helpers/mongo-helper'
 import { Collection } from 'mongodb'
-import request from 'supertest'
+
 import app from '../config/app'
+import env from '@main/config/env'
 
 let surveyCollection: Collection
+let accountCollection: Collection
 
 describe('Surver Routes', () => {
   beforeAll(async () => {
@@ -15,6 +19,8 @@ describe('Surver Routes', () => {
   beforeEach(async () => {
     surveyCollection = MongoHelper.getCollection('surveys')
     await surveyCollection.deleteMany({}) // delete objects in memory so that tests don't overlap
+    accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
 
   describe('POST /surveys', () => {
@@ -23,14 +29,55 @@ describe('Surver Routes', () => {
         .post('/api/surveys')
         .send({
           question: 'Question',
-          answers: [{
-            answer: 'Answer 1',
-            image: 'http://image-name.com'
-          }, {
-            answer: 'Answer 2'
-          }]
+          answers: [
+            {
+              answer: 'Answer 1',
+              image: 'http://image-name.com'
+            },
+            {
+              answer: 'Answer 2'
+            }
+          ]
         })
         .expect(403)
+    })
+
+    test('Should return 204 on add survey with valid accessToken', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'Rodrigo',
+        email: 'rodrigo.maguino@gmail.com',
+        password: '123',
+        role: 'admin'
+      })
+      const id = res.insertedId
+
+      const accessToken = sign(id.toString(), env.jwtSecret)
+
+      await accountCollection.updateOne(
+        { _id: id },
+        {
+          $set: {
+            accessToken
+          }
+        }
+      )
+
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'Question',
+          answers: [
+            {
+              answer: 'Answer 1',
+              image: 'http://image-name.com'
+            },
+            {
+              answer: 'Answer 2'
+            }
+          ]
+        })
+        .expect(204)
     })
   })
 })
