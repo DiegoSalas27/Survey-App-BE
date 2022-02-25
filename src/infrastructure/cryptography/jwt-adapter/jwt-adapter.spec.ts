5017cc3c63b918ddaade7b8de8202e2a344a1ff9
dominicsc2 <dominicsc2hs@gmail.com>
2022-02-24 21:17:46 -0600

    test: ensure JwtAdapter thros if verify throws

--- src/infrastructure/cryptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infrastructure/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -53,5 +53,14 @@ describe('JWT Adapter', () => {
       const value = await sut.decrypt('any_token')
       expect(value).toBe('any_value')
     })
+
+    test('Should throw if verify throws', async () => {
+      const sut = makeSut()
+      jest
+        .spyOn(jwt, 'verify')
+        .mockImplementationOnce(async () => await new Promise((resolve, reject) => reject(new Error())))
+      const promise = sut.decrypt('any_token')
+      await expect(promise).rejects.toThrow()
+    })
   })
 })
