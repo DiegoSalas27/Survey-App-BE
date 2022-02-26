39936179e20ac87315497386c0cda2db01190739
dominicsc2 <dominicsc2hs@gmail.com>
2022-02-26 16:05:32 -0600

    feat: ensure SaveSurveyResult returns 500 if LoadSurveyById throws

--- src/presentation/controller/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controller/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -1,15 +1,19 @@
 import { LoadSurveyById } from '@domain/usecases/survey/load-survey-by-id'
-import { forbidden, InvalidParamError } from '@presentation/middlewares/auth-middleware-protocols'
+import { forbidden, InvalidParamError, serverError } from '@presentation/middlewares/auth-middleware-protocols'
 import { Controller, HttpRequest, httpResponse } from './save-survey-result-controller-protocols'
 
 export class SaveSurveyResult implements Controller {
   constructor(private readonly loadSurveyById: LoadSurveyById) {}
 
   async handle(httpRequest: HttpRequest): Promise<httpResponse> {
-    const survey = await this.loadSurveyById.loadById(httpRequest.params.surveyId)
-    if (!survey) {
-      return forbidden(new InvalidParamError('surveyId'))
+    try {
+      const survey = await this.loadSurveyById.loadById(httpRequest.params.surveyId)
+      if (!survey) {
+        return forbidden(new InvalidParamError('surveyId'))
+      }
+      return null
+    } catch (error: any) {
+      return serverError(error)
     }
-    return null
   }
 }
