1574d41bbe30b9e521cd31035d8b7d6b7b4c7dfd
dominicsc2 <dominicsc2hs@gmail.com>
2022-02-26 10:07:51 -0600

    feat: ensure DbLoadSurveyById calls LoadSurveyByIdRepository with correct id

--- src/main/factories/usecases/survey/load-surveys/db-load-surveys-factory.ts
+++ src/main/factories/usecases/survey/load-surveys/db-load-surveys-factory.ts
@@ -1,4 +1,4 @@
-import { DbLoadSurveys } from '@data/usecases/load-surverys/db-load-surveys'
+import { DbLoadSurveys } from '@data/usecases/load-surveys/db-load-surveys'
 import { LoadSurveys } from '@domain/usecases/load-surveys'
 import { SurveyMongoRepository } from '@infrastructure/db/mongodb/survey/survey-mongo-repository'
 
