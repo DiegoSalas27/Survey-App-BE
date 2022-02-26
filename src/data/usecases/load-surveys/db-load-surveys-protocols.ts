0e599068736ca9194b3aafb5f57bc408f9cf3177
dominicsc2 <dominicsc2hs@gmail.com>
2022-02-26 10:50:53 -0600

    feat: ensure DbSaveSurveyResult calls SaveSurveyResultRepository with correct values

--- src/data/usecases/load-surveys/db-load-surveys-protocols.ts
+++ src/data/usecases/load-surveys/db-load-surveys-protocols.ts
@@ -1,4 +1,4 @@
-export * from '@data/protocols/db/survey/db-load-survey-repository'
+export * from '@data/protocols/db/survey/load-survey-repository'
 export * from '@domain/models/survey'
 export * from '@domain/usecases/load-surveys'
 export * from './db-load-surveys'
--- src/infrastructure/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infrastructure/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,4 +1,4 @@
-import { LoadSurveysRepository } from '@data/protocols/db/survey/db-load-survey-repository'
+import { LoadSurveysRepository } from '@data/protocols/db/survey/load-survey-repository'
 import { AddSurveyModel, AddSurveyRepository } from '@data/usecases/add-survey/db-add-survey-protocols'
 import { SurveyModel } from '@domain/models/survey'
 import { MongoHelper } from '../helpers/mongo-helper'
