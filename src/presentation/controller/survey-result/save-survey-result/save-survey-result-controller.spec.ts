a92d022fd26f18d165bc915ea98d932fbc34794a
dominicsc2 <dominicsc2hs@gmail.com>
2022-02-26 15:54:26 -0600

    test: ensure SaveSurveyResultController calls LoadSurveyById with correct values

---
+++ src/presentation/controller/survey-result/save-survey-result/save-survey-result-controller.spec.ts
@@ -0,0 +1,63 @@
+import { SurveyModel } from '@domain/models/survey'
+import { LoadSurveyById } from '@domain/usecases/survey/load-survey-by-id'
+import { SaveSurveyResult } from './save-survey-result-controller'
+import { HttpRequest } from './save-survey-result-controller-protocols'
+
+const makeFakeRequest = (): HttpRequest => ({
+  params: {
+    surveyId: 'any_survey_id'
+  }
+})
+
+const makeFakeSurvey = (): SurveyModel => ({
+  id: 'any_id',
+  question: 'any_question',
+  answers: [
+    {
+      image: 'any_image',
+      answer: 'any_answer'
+    }
+  ],
+  date: new Date()
+})
+
+const makeLoadSurveyById = (): LoadSurveyById => {
+  class LoadSurveyByIdStub implements LoadSurveyById {
+    async loadById(id: string): Promise<SurveyModel> {
+      return await new Promise(resolve => resolve(makeFakeSurvey()))
+    }
+  }
+
+  return new LoadSurveyByIdStub()
+}
+
+type SutTypes = {
+  sut: SaveSurveyResult
+  loadSurveyByIdStub: LoadSurveyById
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyByIdStub = makeLoadSurveyById()
+  const sut = new SaveSurveyResult(loadSurveyByIdStub)
+
+  return {
+    sut,
+    loadSurveyByIdStub
+  }
+}
+
+describe('SaveSurveyResult Controller', () => {
+  test('Should call LoadSurveyById with correct values', async () => {
+    const { loadSurveyByIdStub, sut } = makeSut()
+    const loadByIdSpy = jest.spyOn(loadSurveyByIdStub, 'loadById')
+    await sut.handle(makeFakeRequest())
+    expect(loadByIdSpy).toHaveBeenCalledWith('any_survey_id')
+  })
+
+    // test('Should return 403 if LoadSurveyById returns null', async () => {
+    //   const { loadSurveyByIdStub, sut } = makeSut()
+    //   jest.spyOn(loadSurveyByIdStub, 'loadById').mockReturnValueOnce(new Promise(resolve => resolve(null)))
+    //   const httpResponse = await sut.handle(makeFakeRequest())
+    //   expect(httpResponse).toBe(403)
+    // })
+})
