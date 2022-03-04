e6d3f2cd5cc54b981ef0ebf0b093a903b5a7051d
dominicsc2 <dominicsc2hs@gmail.com>
2022-03-04 08:30:04 -0600

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+    openapi: '3.0.0',
+    info: {
+        title: 'Clean Node API',
+        descrption: 'API for creating polls between programmers',
+        version: '1.0.0'
+    }
+}
