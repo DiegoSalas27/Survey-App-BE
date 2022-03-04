d1c1ca3394eeb69a159bc42aa5560d27ad21f891
dominicsc2 <dominicsc2hs@gmail.com>
2022-03-04 09:31:13 -0600

    docs: update login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,39 @@
+import { badRequest, unauthorized, serverError } from './components'
+import { loginPath } from './paths'
+import { accountSchema, loginParamsSchema, errorSchema } from './schemas'
+
 export default {
-    openapi: '3.0.0',
-    info: {
-        title: 'Clean Node API',
-        descrption: 'API for creating polls between programmers',
-        version: '1.0.0'
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean Node API',
+    descrption: 'API for creating polls between programmers',
+    version: '1.0.0'
+  },
+  license: {
+    name: 'ISC',
+    url: 'https://opensource.org/licenses/ISC'
+  },
+  servers: [
+    {
+      url: '/api'
     }
+  ],
+  tags: [
+    {
+      name: 'Login'
+    }
+  ],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema,
+    error: errorSchema
+  },
+  components: {
+    badRequest,
+    unauthorized,
+    serverError
+  }
 }
---
+++ src/main/docs/paths/login-path.ts
@@ -0,0 +1,36 @@
+export const loginPath = {
+  post: {
+    tags: ['Login'],
+    summary: 'API for authenticate user',
+    requestBody: {
+      content: {
+        'application/json': {
+          schema: {
+            $ref: '#/schemas/loginParams'
+          }
+        }
+      }
+    },
+    responses: {
+      200: {
+        description: 'Successful',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/account'
+            }
+          }
+        }
+      },
+      400: {
+        $ref: '#/components/badRequest'
+      },
+      401: {
+        $ref: '#/components/unauthorized'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      },
+    }
+  }
+}
