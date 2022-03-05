1fdfa286f195752cae035f76d51f8b24d6e14c17
dominicsc2 <dominicsc2hs@gmail.com>
2022-03-05 10:02:19 -0600

    test: ensure DbLoadSurveyResult returns a SurveyResultModel on success

--- src/data/usecases/load-survey-result/db-load-survey-result.spec.ts
+++ src/data/usecases/load-survey-result/db-load-survey-result.spec.ts
@@ -1,4 +1,4 @@
-import { throwError } from '@domain/test'
+import { mockSurveyResultModel, throwError } from '@domain/test'
 import { DbLoadSurveyResult } from './db-load-survey-result'
 import { LoadSurveyResultRepository, mockLoadSurveyResultRepository } from './db-load-survey-result-protocols'
 
@@ -30,5 +30,11 @@ describe('DbLoadSurveyResult UseCase', () => {
     jest.spyOn(loadSurveyResultRepositoryStub, 'loadBySurveyId').mockImplementationOnce(throwError)
     const promise = sut.load('any_survey_id')
     await expect(promise).rejects.toThrow()
+  })
+
+  test('Should return a SurveyResultModel on success', async () => {
+    const { sut } = makeSut()
+    const surveyResult = await sut.load('any_survey_id')
+    expect(surveyResult).toEqual(mockSurveyResultModel())
   })
 })
