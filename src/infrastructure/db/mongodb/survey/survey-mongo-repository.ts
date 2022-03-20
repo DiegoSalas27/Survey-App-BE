8945f40d370fbabc5eaf364bce32b7765bdf7e21
dominicsc2 <dominicsc2hs@gmail.com>
2022-03-20 00:03:59 -0500

    feat: change load surveys to add didAnswer field

--- src/infrastructure/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infrastructure/db/mongodb/survey/survey-mongo-repository.ts
@@ -3,6 +3,7 @@ import { LoadSurveysRepository } from '@data/protocols/db/survey/load-survey-rep
 import { AddSurveyParams, AddSurveyRepository } from '@data/usecases/survey/add-survey/db-add-survey-protocols'
 import { SurveyModel } from '@domain/models/survey'
 import { ObjectId } from 'mongodb'
+import { QueryBuilder } from '../helpers'
 import { MongoHelper } from '../helpers/mongo-helper'
 
 export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository, LoadSurveyByIdRepository {
@@ -11,10 +12,38 @@ export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRe
     await surveyCollection.insertOne(surveyData)
   }
 
-  async loadAll(): Promise<SurveyModel[]> {
+  async loadAll(accountId: string): Promise<SurveyModel[]> {
     const surveyCollection = MongoHelper.getCollection('surveys')
-    const surveys = await surveyCollection.find().toArray()
-    return MongoHelper.mapArray(surveys)
+    const query = new QueryBuilder()
+      .lookup({
+        from: 'surveyResults',
+        foreignField: 'surveyId',
+        localField: '_id',
+        as: 'result'
+      })
+      .project({
+        _id: 1,
+        question: 1,
+        answers: 1,
+        date: 1,
+        didAnswer: {
+          $gte: [{
+            $size: {
+              $filter: {
+                $input: '$result',
+                as: 'item',
+                cond: {
+                  $eq: ['$$item.accountId', new ObjectId(accountId)]
+                }
+              }
+            }
+          }, 1]
+        }
+      })
+      .build()
+
+      const surveys = await surveyCollection.aggregate(query).toArray()
+      return MongoHelper.mapArray(surveys)
   }
 
   async loadById(id: string): Promise<SurveyModel> {
